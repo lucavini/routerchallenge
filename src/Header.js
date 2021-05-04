f20c162e5591d1016ca042ad2de5e000d4ae7ec2
Lucas Vinicius <lucass.lv.lv@gmail.com>
2021-05-04 12:51:51 -0300

    estiização dos botões de navegação

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./styles/Header.css";
 
 const Header = () => {
+  const [selected, setSelected] = React.useState("Produtos");
+
+  function handelClick({ target }) {
+    setSelected(target.innerText);
+  }
+
   return (
     <React.Fragment>
       <div className="navigation-button">
         <Link to="/">
-          <button className="btn-nav">Produtos</button>
+          <button onClick={handelClick} className={selected==='Produtos'? 'selected':''}  >
+            Produtos
+          </button>
         </Link>
         <Link to="contatos">
-          <button className="btn-nav">Contatos</button>
+          <button onClick={handelClick} className={selected==='Contatos'? 'selected':''} >
+            Contatos
+          </button>
         </Link>
       </div>
     </React.Fragment>
